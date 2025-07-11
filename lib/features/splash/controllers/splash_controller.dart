87a6af26cbd133b8c9ad9c0765b04ceab17d946f
Mosaidur <asifbhuiyan135@gmail.com>
2025-07-11 06:14:57 +0600

    splash screen

--- lib/features/splash/controllers/splash_controller.dart
+++ lib/features/splash/controllers/splash_controller.dart
@@ -6,7 +6,7 @@ class SplashController extends GetxController {
   void onInit() {
     super.onInit();
     Future.delayed(const Duration(seconds: 2), () {
-      Get.offAllNamed(AppRoutes.home);
+      Get.offAllNamed(AppRoutes.createAccountPage);
     });
   }
 }
--- lib/features/splash/screens/splash_screen.dart
+++ lib/features/splash/screens/splash_screen.dart
@@ -1,21 +1,65 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import '../controllers/splash_controller.dart';
 
-class SplashScreen extends GetView<SplashController> {
+class SplashScreen extends StatelessWidget {
   const SplashScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text(
-          'Premier League Lotto',
-          style: Theme.of(context).textTheme.headlineMedium?.copyWith(
-            fontWeight: FontWeight.bold,
-            color: Colors.blue,
+      body: Stack(
+        children: [
+          // Background Image
+          Positioned.fill(
+            child: Image.asset(
+              'assets/images/splash_screen.png',
+              fit: BoxFit.cover,
+            ),
           ),
-        ),
+
+          // Positioned Circular Container
+          Positioned(
+            left: -205,
+            top: -538,
+            child: Container(
+              height: 786,
+              width: 786,
+              decoration: BoxDecoration(
+                color: const Color(0xFFFFFFFF).withOpacity(0.46),
+                shape: BoxShape.circle,
+              ),
+            ),
+          ),
+
+          Positioned(
+            top: 20, // adjust as needed
+            left: 0,
+            right: 0,
+            child: Image.asset(
+              'assets/images/logo_1.png',
+              width: 133,
+              height: 143,
+            ),
+          ),
+          Positioned(
+            top: 133, // adjust as needed
+            left: 0,
+            right: 0,
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+
+                const SizedBox(height: 16),
+                Image.asset(
+                  'assets/images/logo_2.png',
+                  width: 257,
+                ),
+              ],
+            ),
+          )
+
+        ],
       ),
     );
   }
