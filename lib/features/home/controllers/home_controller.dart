08697962c30d3ea55a588522ac0e1cfa871d47d0
Mosaidur <asifbhuiyan135@gmail.com>
2025-07-15 06:11:59 +0600

    home screen page

--- lib/features/home/controllers/home_controller.dart
+++ lib/features/home/controllers/home_controller.dart
@@ -1,30 +1,9 @@
 import 'package:get/get.dart';
 
-import '../models/match_model.dart';
-import '../services/home_service.dart';
-
-
 class HomeController extends GetxController {
-  final HomeService service;
+  var currentIndex = 0.obs;
 
-  HomeController(this.service);
-
-  var matches = <MatchModel>[].obs;
-  var isLoading = false.obs;
-
-  @override
-  void onInit() {
-    super.onInit();
-    loadMatches();
-  }
-
-  void loadMatches() async {
-    isLoading.value = true;
-
-    await Future.delayed(const Duration(seconds: 1)); // simulate delay
-
-    matches.value = service.getMatches();
-
-    isLoading.value = false;
+  void changeTab(int index) {
+    currentIndex.value = index;
   }
 }
--- lib/features/home/screens/home_screen.dart
+++ lib/features/home/screens/home_screen.dart
@@ -1,32 +1,64 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+
 import '../controllers/home_controller.dart';
 
-class HomeScreen extends GetView<HomeController> {
-  const HomeScreen({super.key});
+
+
+class HomePage extends StatelessWidget {
+  final HomeController controller = Get.put(HomeController());
+
+  HomePage({super.key});
+
+  final List<Widget> pages = [
+    HomePage(),
+    Center(child: Text("Meter", style: TextStyle(color: Colors.white))),
+    Center(child: Text("Statistics", style: TextStyle(color: Colors.white))),
+    Center(child: Text("Ticket", style: TextStyle(color: Colors.white))),
+  ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Home - Premier League Lotto'),
+      backgroundColor: Colors.black, // or your background color
+      body: Obx(() => pages[controller.currentIndex.value]),
+      bottomNavigationBar: Obx(
+            () => Container(
+          decoration: BoxDecoration(
+            color: Colors.black,
+            borderRadius: BorderRadius.vertical(top: Radius.circular(12)),
+          ),
+          child: ClipRRect(
+            borderRadius: BorderRadius.vertical(top: Radius.circular(12)),
+            child: BottomNavigationBar(
+              backgroundColor: Colors.black,
+              currentIndex: controller.currentIndex.value,
+              onTap: controller.changeTab,
+              type: BottomNavigationBarType.fixed,
+              selectedItemColor: const Color(0xFF0B3666),
+              unselectedItemColor: Colors.white,
+              items: const [
+                BottomNavigationBarItem(
+                  icon: Icon(Icons.home),
+                  label: "Home",
+                ),
+                BottomNavigationBarItem(
+                  icon: Icon(Icons.speed),
+                  label: "Meter",
+                ),
+                BottomNavigationBarItem(
+                  icon: Icon(Icons.bar_chart),
+                  label: "Statistics",
+                ),
+                BottomNavigationBarItem(
+                  icon: Icon(Icons.confirmation_number),
+                  label: "Ticket",
+                ),
+              ],
+            ),
+          ),
+        ),
       ),
-      body: Obx(() {
-        if (controller.isLoading.value) {
-          return const Center(child: CircularProgressIndicator());
-        }
-
-        return ListView.builder(
-          itemCount: controller.matches.length,
-          itemBuilder: (context, index) {
-            final match = controller.matches[index];
-            return ListTile(
-              title: Text('${match.teamA} vs ${match.teamB}'),
-              subtitle: Text('Match ID: ${match.id}'),
-            );
-          },
-        );
-      }),
     );
   }
 }
